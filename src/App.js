604be56af2ce59bc5ec5e356991095b285a6d247
sayeed <sayeed0209@gmail.com>
2022-11-21 21:06:37 +0100

    React router dom has been implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { useFetchSpotify } from './useFetchSpotify';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import SingleArtistDetails from './pages/SingleArtistDetails';
+import Navbar from './components/Navbar';
 const App = () => {
-	const { isLoading, albums } = useFetchSpotify();
-	return <div>App</div>;
+	return (
+		<Router>
+			<Navbar />
+			<main>
+				<Container>
+					<Routes>
+						<Route index element={<Homepage />} />
+						<Route path="/artist/:id" element={<SingleArtistDetails />} />
+					</Routes>
+				</Container>
+			</main>
+		</Router>
+	);
 };
 
 export default App;
