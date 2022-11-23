54fb039773e3415524503defffccaf8832e4c069
sayeed <sayeed0209@gmail.com>
2022-11-23 17:15:32 +0100

    Default source link provided when images length is zero

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -7,6 +7,10 @@ const Artists = ({ id, name, images, uri }) => {
 	if (images.length > 0) {
 		imgSrc = images[0].url;
 	}
+	if (images.length === 0) {
+		imgSrc =
+			'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+	}
 	return (
 		<Col>
 			<Card className="h-100">
