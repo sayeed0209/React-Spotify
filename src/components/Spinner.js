aa4e358e295ae1fbf591ed3bad6dfbe30c922049
sayeed <sayeed0209@gmail.com>
2022-11-23 17:04:03 +0100

    Loading spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,13 @@ import { Spinner as LoadingSpinner } from 'react-bootstrap';
 
 const Spinner = () => {
 	return (
-		<LoadingSpinner animation="border" role="status">
+		<LoadingSpinner
+			animation="border"
+			role="status"
+			size="lg"
+			variant="warning"
+			style={{ width: '4rem', height: '4rem' }}
+		>
 			<span className="visually-hidden">Loading...</span>
 		</LoadingSpinner>
 	);
