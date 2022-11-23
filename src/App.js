97d0ccfc84099304b294272d59fe258d658090e0
sayeed <sayeed0209@gmail.com>
2022-11-23 16:58:03 +0100

    passing search term via props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,14 +6,26 @@ import Homepage from './pages/Homepage';
 import SingleArtistDetails from './pages/SingleArtistDetails';
 import Navbar from './components/Navbar';
 const App = () => {
+	const [searchTerm, setSearchTerm] = useState(() => 'Taylor Swift');
 	return (
 		<Router>
 			<Navbar />
 			<main>
 				<Container>
 					<Routes>
-						<Route index element={<Homepage />} />
-						<Route path="/artist/:id" element={<SingleArtistDetails />} />
+						<Route
+							index
+							element={
+								<Homepage
+									searchTerm={searchTerm}
+									setSearchTerm={setSearchTerm}
+								/>
+							}
+						/>
+						<Route
+							path="/artist/:id"
+							element={<SingleArtistDetails searchTerm={searchTerm} />}
+						/>
 					</Routes>
 				</Container>
 			</main>
